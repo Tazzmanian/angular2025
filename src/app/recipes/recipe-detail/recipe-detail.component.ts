ffff190c7fd509952e531cc3d23858df6f30ce50
Tazzmanian <banskaliqtabg@yahoo.com>
2025-07-25 18:20:12 +0300

    recipe details

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeDetailComponent implements OnInit {
 
+  dropdownOpen = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleDropdown(event: MouseEvent) {
+    event.preventDefault();
+    this.dropdownOpen = !this.dropdownOpen;
   }
 
 }
