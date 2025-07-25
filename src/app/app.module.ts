d4f7ec662256b27b55a46bb24a9a40b4b2119cf6
Tazzmanian <banskaliqtabg@yahoo.com>
2025-07-25 16:24:22 +0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { RecipeListComponent } from './recepies/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recepies/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recepies/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShopppingEditComponent } from './shopping-list/shoppping-edit/shoppping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecepiesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShopppingEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shopping-list/shoppping-edit/shoppping-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shoppping-edit',
+  templateUrl: './shoppping-edit.component.html',
+  styleUrls: ['./shoppping-edit.component.scss']
+})
+export class ShopppingEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
