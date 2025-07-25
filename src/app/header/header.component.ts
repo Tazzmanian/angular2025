42ff4e0586bc0e8c3c100f6f83fefc414023c6a4
Tazzmanian <banskaliqtabg@yahoo.com>
2025-07-25 17:50:18 +0300

    add recipe list and fix the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  dropdownOpen = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleDropdown(event: MouseEvent) {
+    event.preventDefault();
+    this.dropdownOpen = !this.dropdownOpen;
   }
 
 }
